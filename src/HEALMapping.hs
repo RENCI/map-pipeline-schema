6241e4bfabe8208b31d8a341255d6ebee6a6b752
Hao Xu <xuh@cs.unc.edu>
2019-04-18 17:14:47 -0400

    update

--- src/HEALMapping.hs
+++ src/HEALMapping.hs
@@ -13,13 +13,22 @@ import qualified Data.Text as T
 import Data.List (groupBy)
 import SQLGen
 
+data RandomizationFeature = FirstName | LastName | Name | Id | Email | PhoneNumber | LongTitle | ShortTitle | None
+
 data Item = Item {
     fieldNameHEAL :: !Text,
     fieldNamePhase1 :: !Text,
     dataType :: !SQLType,
+    randomizationFeature :: !RandomizationFeature,
+    dropdownOptions :: !Text,
     lookupNeeded :: !Bool,
+    lookupInformation :: !Text,
     algorithm :: !Text,
     key :: !Text,
+    primary :: !Bool,
+    foreign :: !Bool,
+    foreignKeyTable :: !Text,
+    cardinality :: !Text,
     tableHeal :: !Text,
     tablePhase1 :: !Text,
     nonNull :: !Bool,
@@ -44,15 +53,34 @@ instance FromField SQLType where
     parseField n | BS.take 4 n == "text" = pure SQLVarchar
     parseField n = error ("cannot convert to SQLType " ++ unpack n)
 
+instance FromField RandomizationFeature where
+    parseField "firstname" = FirstName
+    parseField "lastname" = LastName
+    parseField "name" = Name
+    parseField "id" = Id
+    parseField "email" = Email
+    parseField "phonenumber" = PhoneNumber
+    parseField "shorttitle" = ShortTitle
+    parseField "longtitle" = LongTitle
+    parseField "" = None
+
+
 instance FromNamedRecord Item where
     parseNamedRecord m =
         Item
             <$> m .: "Fieldname_HEAL"
             <*> m .: "Fieldname_phase1"
             <*> m .: "Data Type"
+            <*> m .: "Randomization_feature"
+            <*> m .: "Dropdown Options"
             <*> m .: "Lookup Needed"
+            <*> m .: "Lookup Information"
             <*> m .: "Algorithm"
             <*> m .: "Key"
+            <*> m .: "Primary"
+            <*> m .: "Foreign"
+            <*> m .: "FK_tablename"
+            <*> m .: "Cardinality (Table_HEAL--FK_tablename)"
             <*> m .: "Table_HEAL"
             <*> m .: "Table_phase1"
             <*> m .: "NOT NULL"
